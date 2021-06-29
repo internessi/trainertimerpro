6da0511e873011ff59ba85b385a2ef6e8a48c737
Frank Wolf <nessi@internessi.de>
2021-06-29 19:53:20 +0200

    Create basrutten_mp3.dart

---
+++ lib/trainer/basrutten_mp3.dart
@@ -0,0 +1,43 @@
+class BasRuttenMp3 {
+  List<String> _mp3 = [
+    'mp3/basrutten/getreadyin5.mp3',
+    'mp3/basrutten/round1.mp3',
+    'mp3/basrutten/four.mp3',
+    'mp3/basrutten/jab.mp3',
+    'mp3/basrutten/rightstraightlefthook.mp3',
+    'mp3/basrutten/rightstraightlefthookrightstraight.mp3',
+    'mp3/basrutten/lefthookrightuppercut.mp3',
+    'mp3/basrutten/lefthookrightuppercutlefthook.mp3',
+    'mp3/basrutten/threeandalefthook.mp3',
+    'mp3/basrutten/threeandalefthookrightstraight.mp3',
+    'mp3/basrutten/twoandarightstraight.mp3',
+    'mp3/basrutten/fourandarightstraight.mp3',
+    'mp3/basrutten/threehooks.mp3',
+    'mp3/basrutten/twoandathree.mp3',
+    'mp3/basrutten/onetwo.mp3',
+  ];
+  get mp3 => _mp3;
+
+  List<int> _mp3duration = [
+  7,
+  2,
+  1,
+  1,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  2,
+  1,
+  2,
+  1,
+  ];
+
+  get mp3duration => _mp3duration;
+
+}
+
+
