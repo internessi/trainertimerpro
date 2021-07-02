dc3721d67e775de2f2583994d72f187f0461ee93
Frank Wolf <nessi@internessi.de>
2021-07-02 14:18:58 +0200

    Update basrutten_mp3.dart

--- lib/trainer/basrutten_mp3.dart
+++ lib/trainer/basrutten_mp3.dart
@@ -1,31 +1,47 @@
-class BasRuttenMp3 {
 
-  List<String> _mp3 = [
-    'mp3/basrutten/getreadyin5.mp3',
-    'mp3/basrutten/round1.mp3',
-    'mp3/basrutten/four.mp3',
-    'mp3/basrutten/jab.mp3',
-    'mp3/basrutten/rightstraightlefthook.mp3',
-    'mp3/basrutten/rightstraightlefthookrightstraight.mp3',
-    'mp3/basrutten/lefthookrightuppercut.mp3',
-    'mp3/basrutten/lefthookrightuppercutlefthook.mp3',
-    'mp3/basrutten/threeandalefthook.mp3',
-    'mp3/basrutten/threeandalefthookrightstraight.mp3',
-    'mp3/basrutten/twoandarightstraight.mp3',
-    'mp3/basrutten/fourandarightstraight.mp3',
-    'mp3/basrutten/threehooks.mp3',
-    'mp3/basrutten/twoandathree.mp3',
-    'mp3/basrutten/onetwo.mp3',
+  final mp3BasRutten = const [
+    {
+      'mp3Link': 'das liest keiner',
+      'mp3Values': [7, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/getreadyin5.mp3',
+      'mp3Values': [7, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/round1.mp3',
+      'mp3Values': [2, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/four.mp3',
+      'mp3Values': [1, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/getreadyin5.mp3',
+      'mp3Values': [7, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/round1.mp3',
+      'mp3Values': [2, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/four.mp3',
+      'mp3Values': [1, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/getreadyin5.mp3',
+      'mp3Values': [7, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/round1.mp3',
+      'mp3Values': [2, 0, 0, 0],
+    },
+    {
+      'mp3Link': 'mp3/basrutten/four.mp3',
+      'mp3Values': [1, 0, 0, 0],
+    },
   ];
-  get mp3 => _mp3;
 
-  List<int> _mp3duration = [
-  7, 2, 1, 1, 2,
-  2, 2, 2, 2, 2,
-  2, 2, 1, 2, 1,
-  ];
-  get mp3duration => _mp3duration;
 
-}
 
 
